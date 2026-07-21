91a61f99e54a41214d09b2d40c15634bc61567f4
amanb15 <36593444+amanb15@users.noreply.github.com>
2026-07-21 07:16:49 +0530

    Update kong-gateway-dataplanedeploy.sh

--- deploy/kong-gateway-dataplanedeploy.sh
+++ deploy/kong-gateway-dataplanedeploy.sh
@@ -2,12 +2,10 @@
 set -e
 
 ENV_NAME=$1
-CERT_FILE=$2
-KEY_FILE=$3
-if [ -z "$ENV_NAME" ] || [ -z "$CERT_FILE" ] || [ -z "$KEY_FILE" ]; then
-  echo "Usage: ./kong-gateway-dataplanedeploy.sh <env> <tls.crt path> <tls.key path>"
+if [ -z "$ENV_NAME" ]; then
+  echo "Usage: ./kong-gateway-dataplanedeploy.sh <env>"
   exit 1
 fi
 
 ./deploy-kong-operator.sh $ENV_NAME
-./deploy-kong-gateway.sh $ENV_NAME $CERT_FILE $KEY_FILE
+./deploy-kong-gateway.sh $ENV_NAME
